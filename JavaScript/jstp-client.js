9fe9041f102707f565799ce9e214aaf123d68436
o-rumiantsev <alex.rum12.12.12@gmail.com>
2017-12-11 21:14:20 +0200

    multiple interfaces and methods

--- JavaScript/jstp-client.js
+++ JavaScript/jstp-client.js
@@ -3,9 +3,26 @@
 const jstp = require('metarhia-jstp');
 
 jstp.net.connect('jstpApp', null, 8080, 'localhost', (err, conn) => {
-  conn.callMethod('number', 'fortytwo', [], (err, result) => {
-    console.log(result);
+  const args = [10, 9, 8, 7, 6, 5, 4, 3, 2, 1];
+
+  conn.callMethod('operator', 'add', [args], (err, result) => {
+    if (err) console.error(err.message);
+    console.log('add:', result);
   });
+
+  conn.callMethod('operator', 'subtr', [args], (err, result) => {
+    if (err) console.error(err.message);
+    console.log('subtr:', result);
+  });
+
+  conn.callMethod('speaker', 'sayFuck', [], (err, answer) => {
+    console.log(answer);
+  });
+
+  conn.callMethod('speaker', 'sayHello', [], (err, answer) => {
+    console.log(answer);
+  });
+
   stopThisShit(conn);
 });
 
--- JavaScript/jstp-server.js
+++ JavaScript/jstp-server.js
@@ -3,14 +3,34 @@
 const jstp = require('metarhia-jstp');
 const api = {};
 
-api.number = {
-  fortytwo(connection, callback) {
-    callback(null, 42);
+const interfaces = {
+  operator: {
+    add(connection, [...args], callback) {
+      const result = args.reduce((acc, val) => acc + val);
+      callback(null, result);
+    },
+    subtr(connection, args, callback) {
+      const result = args.reduce((acc, val) => val - acc);
+      callback(null, result);
+    }
+  },
+  speaker: {
+    sayFuck(connection, callback) {
+      callback(null, 'fuck');
+    },
+    sayHello(connection, callback) {
+      callback(null, 'hello');
+    }
   }
 }
+
+Object.assign(api, interfaces);
 
 const app = new jstp.Application('jstpApp', api);
 const config = { applications: [app] };
 const server = jstp.net.createServer(config);
 
 server.listen(8080);
+server.on('connection', (socket) => {
+  console.log('Connection');
+});
