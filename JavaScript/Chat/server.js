d4ad2460d1c677f63d999c68ffe15f0ba15d33f7
o-rumiantsev <alex.rum12.12.12@gmail.com>
2017-12-23 00:02:01 +0200

    removed Chat

--- JavaScript/Chat/server.js
+++
@@ -1,9 +0,0 @@
-'use strict';
-
-const jstp = require('metarhia-jstp');
-const app = require('./app.js');
-
-const config = { applications: [app] };
-
-const server = jstp.net.createServer(config);
-server.listen(3000);
